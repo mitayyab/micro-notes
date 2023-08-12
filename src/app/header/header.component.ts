b07293a6f9992a70d356e15bf9f2f6e0cef18d7f
M-Ibrahim-Tayyab <ibrahimtayyab127@gmail.com>
2023-08-12 13:57:24 +0500

    handled navbar toggle

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  isNavbarExpanded:boolean = false;
+  
+  toggleNavbar()
+  {
+    this.isNavbarExpanded = !this.isNavbarExpanded;
+  }
 }
