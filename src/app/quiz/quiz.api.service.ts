055d794e29f0313a9f216b407992eb8a5ee6fa9a
M-Ibrahim-Tayyab <ibrahimtayyab127@gmail.com>
2023-08-17 14:01:29 +0500

    footer corrected and layout added on list component

--- src/app/quiz/quiz.api.service.ts
+++ src/app/quiz/quiz.api.service.ts
@@ -5,32 +5,29 @@ import { Quiz, Question, Option } from 'src/app/quiz/model/quiz';
   providedIn: 'root',
 })
 export class QuizzesService {
-  question2Options = [
-    new Option('Karachi', false),
-    new Option('Islamabad', false),
-    new Option('Faisalabad', true),
-  ];
-
-  question3Options = [
-    new Option('Karachi', true),
-    new Option('Islamabad', false),
-    new Option('Faisalabad', false),
-  ];
-
   readonly quiz: Quiz = new Quiz('Pakistan', [
-    new Question('Which city is the capital of Pakistan?', [
+    new Question(
+      'Which city is the capital of Pakistan?', 
+    [
       new Option('Karachi', false),
       new Option('Islamabad', true),
       new Option('Faisalabad', false),
     ]),
     new Question(
       "Which of the following is known as 'manchester of Pakistan'?",
-      this.question2Options
+      [
+        new Option('Karachi', false),
+        new Option('Islamabad', false),
+        new Option('Faisalabad', true),
+      ]
     ),
     new Question(
-      "Which planet is known as the 'city of lights'?",
-      this.question3Options
-    ),
+      "Which planet is known as the 'city of lights'?", 
+    [
+      new Option('Karachi', true),
+      new Option('Islamabad', false),
+      new Option('Faisalabad', false),
+    ]),
   ]);
 
   getQuiz(): Quiz {
