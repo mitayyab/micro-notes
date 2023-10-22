71e81fa4987b77b64c5b2a50f396fa5fd62f91e1
M-Ibrahim-Tayyab <ibrahimtayyab127@gmail.com>
2023-10-22 21:27:23 +0500

    Simplified validateUsing function and changes function signature to use Handler.

--- backend/src/lib/validator/joi.middleware.ts
+++ backend/src/lib/validator/joi.middleware.ts
@@ -1,25 +1,17 @@
-import { RequestHandler, Request, Response, NextFunction } from 'express';
+import { Handler, Request, Response, NextFunction } from 'express';
 import { Validator } from '@lib/validator/joi';
 import { RequestInputError } from '@lib/error/RequestInputError';
 
-export const validateUsing = <T>(
-   validateInput: Validator<T>
-): RequestHandler => {
-   const handler: RequestHandler = async (
-      req: Request,
-      res: Response,
-      next: NextFunction
-   ) => {
-      const input: T = req.body;
+export const validateUsing =
+   <T>(validateInput: Validator<T>): Handler =>
+      async (req: Request, res: Response, next: NextFunction) => {
+         const input: T = req.body;
 
-      const errors = validateInput(input);
+         const errors = validateInput(input);
 
-      if (errors) {
-         return next(new RequestInputError(errors));
-      }
+         if (errors) {
+            return next(new RequestInputError(errors));
+         }
 
-      next();
-   };
-
-   return handler;
-};
+         next();
+      };
