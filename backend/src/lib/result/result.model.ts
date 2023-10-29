72bb729e50cf5a646ef592bdda8993b09c582e1f
ibrahim <ibrahimtayyab127@gmail.com>
2023-10-29 14:49:35 +0000

    refactor code of POST result handler

--- backend/src/lib/result/result.model.ts
+++ backend/src/lib/result/result.model.ts
@@ -29,55 +29,6 @@ export interface Result extends Document {
    quiz: attemptedQuiz;
 }
 
-// const resultSchemaDefinition = {
-//    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-//    quiz: {
-//       title: { type: String, required: true },
-//       topics: [{ type: String, required: true }],
-//       level: {
-//          type: String,
-//          enum: Level,
-//          required: true,
-//       },
-//       questions: [
-//          {
-//             text: { type: String, required: true },
-//             answerChoices: [
-//                {
-//                   text: { type: String, required: true },
-//                   correct: { type: Boolean, required: true },
-//                   selected: { type: Boolean, required: true },
-//                },
-//             ],
-//          },
-//       ],
-//    },
-// };
-
-// -------------------------------------------------------------------------
-
-// const resultSchemaDefinition = {
-//    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-//    quiz: quizSchemaDefinition,
-// };
-
-// resultSchemaDefinition.quiz.questions = [
-//    Object.assign(
-//       QuestionSchemaDefinition,
-//    ),
-// ];
-
-// resultSchemaDefinition.quiz.questions[0].answerChoices = [
-//    Object.assign(
-//       {
-//          selected: { type: Boolean, required: true },
-//       },
-//       resultSchemaDefinition.quiz.questions[0].answerChoices[0],
-//    ),
-// ];
-
-// -----------------------------------------------------------------------------
-
 export const attemptedAnswerChoicesSchema = Object.assign(
    {
       selected: { type: Boolean, required: true },
@@ -98,26 +49,6 @@ export const resultSchemaDefinition = {
    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
    quiz: attemptedQuizSchema,
 };
-
-// ------------------------------------------------------------------------------
-
-// const resultSchemaDefinition = {
-//    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-//    quiz: quizSchemaDefinition,
-// };
-
-// resultSchemaDefinition.quiz.questions = [
-//    Object.assign(QuestionSchemaDefinition),
-// ];
-
-// resultSchemaDefinition.quiz.questions[0].answerChoices = [
-//    Object.assign(
-//       {
-//          selected: { type: Boolean, required: true },
-//       },
-//       resultSchemaDefinition.quiz.questions[0].answerChoices[0],
-//    ),
-// ];
 
 export const ResultSchema = new mongoose.Schema(resultSchemaDefinition);
 
